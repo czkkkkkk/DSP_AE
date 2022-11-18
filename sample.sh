0aa44dbd92e5ccc1631b672d194d507f55e393dd
czkkkkkk <zekucai@gmail.com>
2022-11-18 08:36:57 +0000

    Redirect script output to files

--- sample.sh
+++ sample.sh
@@ -3,39 +3,44 @@ model="graphsage"
 
 echo dsp_epoch_time
 conda activate dsp
+mkdir -p logs/dsp/sample
 for dataset in "ogb-product" "papers" "friendster"; do
   for rank in 1 2 4 8; do
-    bash dsp/sample.sh $dataset $rank
+    bash dsp/sample.sh $dataset $rank >& logs/dsp/sample/${dataset}_${rank}gpus.txt
   done
 done
 
 conda activate dgl
 echo dgl_cpu_epoch_time
+mkdir -p logs/dgl-cpu/sample
 for dataset in "product" "paper" "fs"; do
   for rank in 1 2 4 8; do
-    bash run.sh $dataset $rank 0 1
+    bash dgl/run.sh $dataset $rank 0 1 >& logs/dgl-cpu/sample/${dataset}_${rank}gpus.txt
   done
 done
 
 echo dgl_uva_epoch_time
+mkdir -p logs/dgl-uva/sample
 for dataset in "product" "paper" "fs"; do
   for rank in 1 2 4 8; do
-    bash run.sh $dataset $rank 1 1
+    bash dgl/run.sh $dataset $rank 1 1 >& logs/dgl-uva/sample/${dataset}_${rank}gpus.txt
   done
 done
 
 conda activate pyg
 echo pyg_epoch_time
+mkdir -p logs/pyg/sample
 for dataset in "product" "paper" "fs"; do
   for rank in 1 2 4 8; do
-    bash run.sh $dataset $rank 1
+    bash pyg/run.sh $dataset $rank 1 >& logs/pyg/sample/${dataset}_${rank}gpus.txt
   done
 done
 
 conda activate quiver
 echo quiver_epoch_time
+mkdir -p logs/quiver/sample
 for dataset in "product" "paper" "fs"; do
   for rank in 1 2 4 8; do
-    bash run.sh $dataset $rank 1
+    bash quiver/run.sh $dataset $rank 1 logs/quiver/sample/${dataset}_${rank}gpus.txt
   done
 done
--- train.sh
+++ train.sh
@@ -3,39 +3,44 @@ model="graphsage"
 
 echo dsp_epoch_time
 conda activate dsp
+mkdir -p logs/dsp/train
 for dataset in "ogb-product" "papers" "friendster"; do
   for rank in 1 2 4 8; do
-    bash dsp/train.sh $dataset $rank
+    bash dsp/train.sh $dataset $rank >& logs/dsp/train/${dataset}_${rank}gpus.txt
   done
 done
 
 conda activate dgl
 echo dgl_cpu_epoch_time
+mkdir -p logs/dgl-cpu/train
 for dataset in "product" "paper" "fs"; do
   for rank in 1 2 4 8; do
-    bash run.sh $dataset $rank 0 0
+    bash dgl/run.sh $dataset $rank 0 0 >& logs/dgl-cpu/train/${dataset}_${rank}gpus.txt
   done
 done
 
 echo dgl_uva_epoch_time
+mkdir -p logs/dgl-uva/train
 for dataset in "product" "paper" "fs"; do
   for rank in 1 2 4 8; do
-    bash run.sh $dataset $rank 1 0
+    bash run.sh $dataset $rank 1 0 >& logs/dgl-uva/train/${dataset}_${rank}gpus.txt
   done
 done
 
 conda activate pyg
 echo pyg_epoch_time
+mkdir -p logs/pyg/train
 for dataset in "product" "paper" "fs"; do
   for rank in 1 2 4 8; do
-    bash run.sh $dataset $rank 0
+    bash run.sh $dataset $rank 0 >& logs/pyg/train/${dataset}_${rank}gpus.txt
   done
 done
 
 conda activate quiver
 echo quiver_epoch_time
+mkdir -p logs/quiver/train
 for dataset in "product" "paper" "fs"; do
   for rank in 1 2 4 8; do
-    bash run.sh $dataset $rank 0
+    bash run.sh $dataset $rank 0 >& logs/quiver/train/${dataset}_${rank}gpus.txt
   done
 done
