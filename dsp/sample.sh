bc63f58b3f4878b5755067c303a5f4d7af93d5a3
czkkkkkk <zekucai@gmail.com>
2022-11-18 08:47:05 +0000

    Update dsp scripts

--- dsp/sample.sh
+++ dsp/sample.sh
@@ -7,8 +7,33 @@ export DGL_DS_USE_NCCL=1
 export DGL_DS_MASTER_PORT=12210
 export DGL_DS_COMM_PORT=17211
 
-cache_ratio=50
-graph_cache_ratio=100
+echo "--------------Running DSP sampler on $dataset with $n_gpus GPUs--------------------"
+if [ $dataset == "ogb-product" ]; then
+  graph_cache_ratio=100
+elif [ $dataset == "ogb-paper100M" ]; then
+  if [ $n_gpus == "1" ]; then
+    graph_cache_ratio=20
+  elif [ $n_gpus == "2" ]; then
+    graph_cache_ratio=50
+  elif [ $n_gpus == "4" ]; then
+    graph_cache_ratio=100
+  elif [ $n_gpus == "8" ]; then
+    graph_cache_ratio=100
+  fi
+elif [ $dataset == "friendster" ]; then
+  if [ $n_gpus == "1" ]; then
+    graph_cache_ratio=40
+  elif [ $n_gpus == "2" ]; then
+    graph_cache_ratio=80
+  elif [ $n_gpus == "4" ]; then
+    graph_cache_ratio=100
+  elif [ $n_gpus == "8" ]; then
+    graph_cache_ratio=100
+  fi
+else
+  echo "Invalid dataset: $dataset"
+fi
+
 graph_cache_gb=-1
 
 python /root/projects/DSP_AE/dsp/sampling.py --part_config=${datadir} \
--- dsp/train.sh
+++ dsp/train.sh
@@ -7,11 +7,45 @@ export DGL_DS_USE_NCCL=1
 export DGL_DS_MASTER_PORT=12210
 export DGL_DS_COMM_PORT=17211
 
-cache_ratio=50
-graph_cache_ratio=100
-feat_cache_gb=-1
+feat_mode=PartitionCache
+echo "--------------Running DSP on $dataset with $n_gpus GPUs--------------------"
+if [ $dataset == "ogb-product" ]; then
+  feat_cache_ratio=100
+  graph_cache_ratio=100
+elif [ $dataset == "ogb-paper100M" ]; then
+  if [ $n_gpus == "1" ]; then
+    feat_cache_ratio=5
+    graph_cache_ratio=20
+  elif [ $n_gpus == "2" ]; then
+    feat_cache_ratio=10
+    graph_cache_ratio=20
+  elif [ $n_gpus == "4" ]; then
+    feat_cache_ratio=20
+    graph_cache_ratio=20
+  elif [ $n_gpus == "8" ]; then
+    feat_cache_ratio=50
+    graph_cache_ratio=100
+  fi
+elif [ $dataset == "friendster" ]; then
+  if [ $n_gpus == "1" ]; then
+    feat_cache_ratio=10
+    graph_cache_ratio=0
+  elif [ $n_gpus == "2" ]; then
+    feat_cache_ratio=20
+    graph_cache_ratio=0
+  elif [ $n_gpus == "4" ]; then
+    feat_cache_ratio=40
+    graph_cache_ratio=0
+  elif [ $n_gpus == "8" ]; then
+    feat_cache_ratio=50
+    graph_cache_ratio=20
+  fi
+else
+  echo "Invalid dataset: $dataset"
+fi
 graph_cache_gb=-1
-feat_mode=AllCache
+feat_cache_gb=-1
+
 
 python /root/projects/DSP_AE/dsp/graphsage.py --part_config=${datadir} \
   --n_ranks=${n_gpu} \
