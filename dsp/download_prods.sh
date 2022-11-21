9c2833714ef275c7df4b378fd1e90530ba4dab24
czkkkkkk <zekucai@gmail.com>
2022-11-21 06:20:18 +0000

    Update download script

--- dsp/download_prods.sh
+++ dsp/download_prods.sh
@@ -1,6 +1,8 @@
-for n_gpu in 1 2 4 8
+for n_gpu in 1
 do
-  outdir=/data/dsp/ogb-product${n_gpu}/
+  outdir=/data/dsp
   mkdir -p $outdir
-  aws s3 cp s3://dsp-ae/dsp/ogb-product${n_gpu}/ $outdir  --no-sign-request --recursive
+  aws s3 cp s3://dgl-data/dsp-ae/ogb-product${n_gpu}.tar.gz $outdir  --no-sign-request --recursive
+  cd /data/dsp
+  tar zxvf ogb-product${n_gpu}.tar.gz
 done
